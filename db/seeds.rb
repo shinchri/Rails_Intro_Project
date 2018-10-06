1b6b050f03239996e25d521695e45bc16f57cb7e
shinchri <shinchri7@gmail.com>
2018-10-05 19:00:28 -0500

    Added statement in seeds.rb to properly destroy data in the tabales

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+// remove all data from the database
+Chef.destroy_all
+Restaurant.destroy_all
+Food.destroy_all
